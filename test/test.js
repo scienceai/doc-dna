d3fa7ff21cf458348a6c448274ae4641462f310b
sballesteros <sebastien.ballesteros@gmail.com>
2014-09-24 23:45:35 -0400

    fix tests

--- test/test.js
+++ test/test.js
@@ -9,49 +9,47 @@ var docUnit = JSON.parse(fs.readFileSync(path.join(__dirname, 'fixtures', 'doc_u
 
 describe('docDna', function(){
 
-  describe('compute', function(){
-    it('should compute the data needed for a chord diagram', function(){
-      var expected = {
-        labels: [
-          { name: 'data1', type: 'Dataset' },
-          { name: 'data2', type: 'Dataset' },
-          { name: 'result1', type: 'Dataset' },
-          { name: 'result2', type: 'Dataset' },
-          { name: 'prog', type: 'SoftwareApplication' },
-          { name: 'image1', type: 'ImageObject' },
-          { name: 'image2', type: 'ImageObject' }
-        ],
-        matrix: [
-          [ 0, 1, 1, 0, 1, 1, 0 ], //data1 needed by data2, result1, sourceCode and image1
-          [ 0, 0, 0, 1, 1, 0, 1 ], //data2 needed by result2, sourceCode and image2
-          [ 0, 0, 0, 0, 0, 1, 0 ], //result1 needed by image 1
-          [ 0, 0, 0, 0, 0, 0, 1 ], //result2 needed by image 2
-          [ 0, 0, 1, 1, 0, 1, 1 ], //sourceCode needed by results1, results2, image1 and image2
-          [ 0, 0, 0, 0, 0, 0, 0 ], //image 1 needed by nothing
-          [ 0, 0, 0, 0, 0, 0, 0 ], //image 2 needed by nothing
-        ]
-      };
+  it('should compute the data needed for a chord diagram', function(){
+    var expected = {
+      labels: [
+        { name: 'data1', type: 'Dataset' },
+        { name: 'data2', type: 'Dataset' },
+        { name: 'result1', type: 'Dataset' },
+        { name: 'result2', type: 'Dataset' },
+        { name: 'prog', type: 'SoftwareApplication' },
+        { name: 'image1', type: 'ImageObject' },
+        { name: 'image2', type: 'ImageObject' }
+      ],
+      matrix: [
+        [ 0, 1, 1, 0, 1, 1, 0 ], //data1 needed by data2, result1, sourceCode and image1
+        [ 0, 0, 0, 1, 1, 0, 1 ], //data2 needed by result2, sourceCode and image2
+        [ 0, 0, 0, 0, 0, 1, 0 ], //result1 needed by image 1
+        [ 0, 0, 0, 0, 0, 0, 1 ], //result2 needed by image 2
+        [ 0, 0, 1, 1, 0, 1, 1 ], //sourceCode needed by results1, results2, image1 and image2
+        [ 0, 0, 0, 0, 0, 0, 0 ], //image 1 needed by nothing
+        [ 0, 0, 0, 0, 0, 0, 0 ], //image 2 needed by nothing
+      ]
+    };
 
-      var data = docDna.compute(docDep);
-      assert.deepEqual(data, expected);
-    });
+    var data = docDna.compute(docDep);
+    assert.deepEqual(data, expected);
+  });
 
-    it('should compute the data needed for a chord diagram in the special case when all the element are independent', function(){
-      var expected = {
-        labels: [
-          { name: 'data', type: 'Dataset' },
-          { name: 'prog', type: 'SoftwareApplication' },
-          { name: 'image', type: 'ImageObject' }
-        ],
-        matrix: [ //diag matrix if no deps
-          [ 1, 0, 0 ],
-          [ 0, 1, 0 ],
-          [ 0, 0, 1 ]
-        ]
-      };
-      var data = docDna.compute(docInd);
-      assert.deepEqual(data, expected);
-    });
+  it('should compute the data needed for a chord diagram in the special case when all the element are independent', function(){
+    var expected = {
+      labels: [
+        { name: 'data', type: 'Dataset' },
+        { name: 'prog', type: 'SoftwareApplication' },
+        { name: 'image', type: 'ImageObject' }
+      ],
+      matrix: [ //diag matrix if no deps
+        [ 1, 0, 0 ],
+        [ 0, 1, 0 ],
+        [ 0, 0, 1 ]
+      ]
+    };
+    var data = docDna.compute(docInd);
+    assert.deepEqual(data, expected);
   });
 
   it('should compute the data needed for a chord diagram when only one resource exists', function(){
